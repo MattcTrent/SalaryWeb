cecb7f92153c30089f57b26ce5fb6ba0d049e124
MattcTrent <matt_trent@hotmail.com>
2022-11-02 21:13:34 +0000

    Changes on UseQueryStuff to stop errors

--- src/Pages/SystemParameters.js
+++ src/Pages/SystemParameters.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Component } from "react";
 import { SystemParameterList } from "../Components/SystemParameters/SystemParameterList.js";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
+const queryClient = new QueryClient()
 
 export class SystemParameters extends Component {
   render() {
     return (
       <>
+        <QueryClientProvider client={queryClient}>
           <SystemParameterList></SystemParameterList>
+        </QueryClientProvider>
       </>
     )
   }
